780c551454f6883b469c4af895faf64ccec589d9
liangfeng2333 <2570635897@qq.com>
2022-01-27 19:50:37 +0800

    first commit

--- static/index.js
+++ static/index.js
@@ -425,11 +425,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
+        if (cps <= 5) return '再接再厉！';
+        if (cps <= 8) return '不错';
+        if (cps <= 10)  return '太强了';
         if (cps <= 15) return '您';
-        return '人？';
+        return '您已经超越人类了！';
     }
 
     function toStr(obj) {
@@ -466,7 +466,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
     function initSetting() {
         $("#username").val(cookie("username") ? cookie("username") : "");
         $("#message").val(cookie("message") ? cookie("message") : "");
-        $("title").text(cookie("title") ? cookie("title") : "吃掉小鹿乃");
+        $("title").text(cookie("title") ? cookie("title") : "Ices");
         let keyboard = cookie('keyboard');
         if (keyboard) {
             keyboard = keyboard.toLowerCase();
